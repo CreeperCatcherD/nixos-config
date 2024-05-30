c840501b4f48253261f8fd553884a63da381814e
CreeperCatcherD <49421694+CreeperCatcherD@users.noreply.github.com>
2024-05-29 19:05:32 -0500

    2024-05-29 19:05:31

--- nixos-config-main/nixos-native/modules/xserver.nix
+++ nixos-config-main/nixos-native/modules/xserver.nix
@@ -3,7 +3,7 @@
       enable = true;
 
       # Enable the KDE Plasma Desktop Environment.
-      #displayManager.sddm.enable = true;
+      displayManager.sddm.enable = true;
       #desktopManager.plasma5.enable = true;
 
       windowManager.herbstluftwm.enable = true;
@@ -23,8 +23,8 @@
         touchpad.accelProfile = "flat";
       };
 
-      #videoDrivers = [ "amdgpu" ];
-      #deviceSection = ''Option "TearFree" "True"'';
+      videoDrivers = [ "nvidia" ];
+      deviceSection = ''Option "TearFree" "True"'';
       #displayManager.gdm.enable = true;
       #desktopManager.gnome.enable = true;
     };
