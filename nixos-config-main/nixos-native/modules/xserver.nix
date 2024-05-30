9881aba1096be7a7d0d5d3c6b32aa6bba7b4fdbb
CreeperCatcherD <49421694+CreeperCatcherD@users.noreply.github.com>
2024-05-29 21:26:19 -0500

    2024-05-29 21:26:19

--- nixos-config-main/nixos-native/modules/xserver.nix
+++ nixos-config-main/nixos-native/modules/xserver.nix
@@ -3,8 +3,8 @@
       enable = true;
 
       # Enable the KDE Plasma Desktop Environment.
-      #displayManager.sddm.enable = true;
-      #desktopManager.plasma5.enable = true;
+      displayManager.sddm.enable = true;
+      desktopManager.plasma5.enable = true;
 
       windowManager.herbstluftwm.enable = true;
 
@@ -28,6 +28,6 @@
       #displayManager.gdm.enable = true;
       #desktopManager.gnome.enable = true;
     };
-    services.displayManager.sddm.enable = true;
-    services.displayManager.sddm.wayland.enable = true;
+    #services.displayManager.sddm.enable = true;
+    #services.displayManager.sddm.wayland.enable = true;
 }
